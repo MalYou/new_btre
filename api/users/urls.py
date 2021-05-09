b27d454960087958ae5fce4dd8d83d3abd30e803
younesm@hpy <younesmalek2014@gmail.com>
2021-05-09 15:04:39 +0200

    fix users app urls

--- api/users/urls.py
+++ api/users/urls.py
@@ -5,7 +5,7 @@ from rest_framework_simplejwt import views as jwt_views
 from . import views
 
 urlpatterns = [
-    path('create/', views.UserCreateView.as_view(), name='create'),
+    path('', views.UserCreateView.as_view(), name='create'),
     path('me/', views.UserRetreiveView.as_view(), name='me'),
     path('token/', jwt_views.TokenObtainPairView.as_view(), name='token'),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='refresh_token'),
